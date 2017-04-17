0cabfb55a085e5c059723e5797a15943f742d1b8
Nevyn Tatum <nevyn202@outlook.com>
2017-04-17 17:41:45 -0500

    6

---
+++ src/main/java/nat/nshm/init/ModItems.java
@@ -0,0 +1,14 @@
+package nat.nshm.init;
+
+public class ModItems {
+	
+	public static void init(){
+		
+	}
+	public static void register(){
+		
+	}
+	public static void registerRenders(){
+		
+	}
+}
